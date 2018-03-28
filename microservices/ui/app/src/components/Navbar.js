0b210acc299ea83e093af1aad825935ffb7e0b0c
Dylan <dmccapes@mac.com>
2018-03-28 16:17:25 -0700

    Add initial navbar

---
+++ microservices/ui/app/src/components/Navbar.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+class Navbar extends Component {
+  render() {
+    return (
+      <div className="nav">
+        <div className="nav-left">
+          <p className="nav-title">tawkify</p>
+          <div className="links">
+            <p>How it works</p>
+            <p>FAQ</p>
+            <p>Stories</p>
+            <p>Sign In</p>
+          </div>
+        </div>
+        <div className="nav-right">
+          <p>1(888)494-7280</p>
+          <div>FREE SCREENING</div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Navbar;
