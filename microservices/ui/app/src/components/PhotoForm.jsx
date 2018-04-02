ffa76ee21539aff670589c5b4f0a6c4a8b2c702d
Dylan <dmccapes@mac.com>
2018-04-02 14:51:52 -0700

    Change style for file input button

--- microservices/ui/app/src/components/PhotoForm.jsx
+++ microservices/ui/app/src/components/PhotoForm.jsx
@@ -29,9 +29,7 @@ class PhotoForm extends Component {
             We ask that you upload at least 2 pictures of yourself; but upload as many as you'd like. We encourage you to review your pictures periodically to make sure they are up to date. Snapshots and Selfies just fine here. Remember this image is for our internal use and will not be shared with potential matches – you will also be able to swap it out later if you wish.
           </div>
           <div className="form-photo-img"></div>
-          <div className="choose-file-btn">
-            CHOOSE FILE
-          </div>
+          <input type="file" className="choose-file-btn" />
           <div className="form-photo-desc">
             Your pictures must be 4 megabytes or smaller. If you have problems, please contact us1l 1 (646) 791-3283
           </div>
