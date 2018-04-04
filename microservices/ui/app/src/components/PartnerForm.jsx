2bbcdb2878bd5bdec7794d4e59ee90eaa444e2c1
Dylan <dmccapes@mac.com>
2018-04-03 20:33:14 -0700

    Set state with DOM elements ideal partner

--- microservices/ui/app/src/components/PartnerForm.jsx
+++ microservices/ui/app/src/components/PartnerForm.jsx
@@ -1,10 +1,77 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
+// URL for database calls
+var url = "https://data.gibber74.hasura-app.io/v1/query";
+
 class PartnerForm extends Component {
+  constructor() {
+    super();
+    this.state = {
+      userInfo: {
+        qualities: [],
+        moreQualities: "",
+        goal: "",
+        moreGoals: "",
+        hangups: [],
+        moreHangups: "",
+        traits: [],
+        moreTraits: "",
+
+      },
+      users: []
+    }
+  }
+
   componentDidMount() {
+
+    const qualities = document.getElementById("qualities");
+    const qualitiesButton = document.getElementById("qualities-button");
+    qualitiesButton.addEventListener('click', () => {
+      this.state.userInfo.qualities.push(qualities.value);
+    });
+
+    const moreQualities = document.getElementById("more-qualities");
+    moreQualities.addEventListener('change', () => {
+      this.state.userInfo.moreQualities = moreQualities.value;
+    });
+
+    const goal = document.getElementById("goal");
+    goal.addEventListener('change', () => {
+      this.state.userInfo.goal = goal.options[goal.selectedIndex].value;
+    });
+
+    const moreGoals = document.getElementById("more-goals");
+    moreGoals.addEventListener('change', () => {
+      this.state.userInfo.moreGoals = moreGoals.value;
+    });
+
+    const hangups = document.getElementById("hangups");
+    const hangupsButton = document.getElementById("hangups-button");
+    hangupsButton.addEventListener('click', () => {
+        this.state.userInfo.hangups.push(hangups.value)
+    });
+
+    const moreHangups = document.getElementById("more-hangups");
+    moreHangups.addEventListener('change', () => {
+      this.state.userInfo.moreHangups = moreQualities.value;
+    });
+
+    const traits = document.getElementById("traits");
+    const traitsButton = document.getElementById("traits-button");
+    traitsButton.addEventListener('click', () => {
+        this.state.userInfo.traits.push(traits.value)
+    });
+
+    const moreTraits = document.getElementById("more-traits");
+    moreTraits.addEventListener('change', () => {
+      this.state.userInfo.moreTraits = moreQualities.value;
+    });
+
+
     const submit = document.getElementById("partner-submit");
     submit.addEventListener('click', () => {
+      console.log(this.state.userInfo);
       this.props.history.push("/addphotos");
     });
   }
@@ -28,10 +95,10 @@ class PartnerForm extends Component {
       Who would you like to meet? The more we know the better we work.
       </div>
 
-      <div id="qualities-prompt" className="input-prompt">TOP QUALITIES IN A MATCH</div>
+      <div id="hangups-prompt" className="input-prompt">TOP QUALITIES IN A MATCH</div>
       <div id="qualities-input-container" className="qualities-input-container">
-        <input id="form-qualities" className="form-input-item" type="text" placeholder="e.g. a good sense of humor"></input>
-        <div id="qualities-button" className="qualities-button"><i class="fa fa-plus"></i><div>ADD</div></div>
+        <input id="qualities" className="form-input-item" type="text" placeholder="e.g. a good sense of humor"></input>
+        <div id="qualities-button" className="qualities-button"><i className="fa fa-plus"></i><div>ADD</div></div>
       </div>
       <div className="input-description">Tell us the qualities you prioritize in an ideal match.</div>
 
@@ -40,7 +107,7 @@ class PartnerForm extends Component {
       <div id="location-desc" className="input-description">Feel free to elaborate on your top qualities in a match.</div>
 
       <div className="input-prompt">YOUR RELATIONSHIP GOAL</div>
-      <select id="form-goal" className="ideal-select">
+      <select id="goal" className="ideal-select">
         <option></option>
         <option>I'm still figuring it all out...</option>
         <option>I don't know what I'm looking for</option>
@@ -55,8 +122,8 @@ class PartnerForm extends Component {
 
       <div className="input-prompt">RELATIONSHIP HANGUPS</div>
       <div id="hangups-input-container" className="qualities-input-container">
-        <input id="form-qualities" className="form-input-item" type="text" placeholder="e.g. long distance"></input>
-        <div id="hangups-button" className="qualities-button"><i class="fa fa-plus"></i><div>ADD</div></div>
+        <input id="hangups" className="form-input-item" type="text" placeholder="e.g. long distance"></input>
+        <div id="hangups-button" className="qualities-button"><i className="fa fa-plus"></i><div>ADD</div></div>
       </div>
       <div className="input-description">Tell us some traits that would make you think twice before committing to a relationship. C'mon! Let's be realistic here.</div>
 
@@ -66,8 +133,8 @@ class PartnerForm extends Component {
 
       <div className="input-prompt">YOUR STANDOUT TRAITS</div>
       <div id="traits-input-container" className="qualities-input-container">
-        <input id="form-traits" className="form-input-item" type="text" placeholder="e.g. established career"></input>
-        <div id="hangups-button" className="qualities-button"><i class="fa fa-plus"></i><div>ADD</div></div>
+        <input id="traits" className="form-input-item" type="text" placeholder="e.g. established career"></input>
+        <div id="traits-button" className="qualities-button"><i className="fa fa-plus"></i><div>ADD</div></div>
       </div>
 
       <div className="input-prompt">IF YOU'D LIKE... TELL US MORE</div>
