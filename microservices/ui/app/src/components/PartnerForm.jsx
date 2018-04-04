00178d36777fe6295d06d2a5e0199eff9088849e
Dylan <dmccapes@mac.com>
2018-04-03 19:26:17 -0700

    Add ideal partner DOM elements

--- microservices/ui/app/src/components/PartnerForm.jsx
+++ microservices/ui/app/src/components/PartnerForm.jsx
@@ -31,13 +31,48 @@ class PartnerForm extends Component {
       <div id="qualities-prompt" className="input-prompt">TOP QUALITIES IN A MATCH</div>
       <div id="qualities-input-container" className="qualities-input-container">
         <input id="form-qualities" className="form-input-item" type="text" placeholder="e.g. a good sense of humor"></input>
-        <div className="qualities-button"><i class="fa fa-plus"></i><div>ADD</div></div>
+        <div id="qualities-button" className="qualities-button"><i class="fa fa-plus"></i><div>ADD</div></div>
       </div>
       <div className="input-description">Tell us the qualities you prioritize in an ideal match.</div>
 
-      <div id="more-qualities" className="input-prompt">IF YOU'D LIKE... TELL US MORE</div>
-      <input id="form-location" className="form-more" type="text"></input>
+      <div className="input-prompt">IF YOU'D LIKE... TELL US MORE</div>
+      <input id="more-qualities" className="form-more" type="text"></input>
       <div id="location-desc" className="input-description">Feel free to elaborate on your top qualities in a match.</div>
+
+      <div className="input-prompt">YOUR RELATIONSHIP GOAL</div>
+      <select id="form-goal" className="ideal-select">
+        <option></option>
+        <option>I'm still figuring it all out...</option>
+        <option>I don't know what I'm looking for</option>
+        <option>I'm ready to get married...</option>
+        <option>I'm ready to start a family...</option>
+        <option>I'm searching for my life partner...</option>
+      </select>
+
+      <div className="input-prompt">IF YOU'D LIKE... TELL US MORE</div>
+      <input id="more-goals" className="form-more" type="text"></input>
+      <div className="input-description">Let's work together on your goals.</div>
+
+      <div className="input-prompt">RELATIONSHIP HANGUPS</div>
+      <div id="hangups-input-container" className="qualities-input-container">
+        <input id="form-qualities" className="form-input-item" type="text" placeholder="e.g. long distance"></input>
+        <div id="hangups-button" className="qualities-button"><i class="fa fa-plus"></i><div>ADD</div></div>
+      </div>
+      <div className="input-description">Tell us some traits that would make you think twice before committing to a relationship. C'mon! Let's be realistic here.</div>
+
+      <div className="input-prompt">IF YOU'D LIKE... TELL US MORE</div>
+      <input id="more-hangups" className="form-more" type="text"></input>
+      <div className="input-description">Why are these hangups important to you?</div>
+
+      <div className="input-prompt">YOUR STANDOUT TRAITS</div>
+      <div id="traits-input-container" className="qualities-input-container">
+        <input id="form-traits" className="form-input-item" type="text" placeholder="e.g. established career"></input>
+        <div id="hangups-button" className="qualities-button"><i class="fa fa-plus"></i><div>ADD</div></div>
+      </div>
+
+      <div className="input-prompt">IF YOU'D LIKE... TELL US MORE</div>
+      <input id="more-traits" className="form-more" type="text"></input>
+      <div className="input-description">Tell us some traits that would make you think twice before committing to a relationship. C'mon! Let's be realistic here.</div>
 
       <div id="partner-submit" className="submit">
       SAVE AND CONTINUE
